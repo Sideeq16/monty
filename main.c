f5f97634f015764b21e3883c01ff36755cbd688f
Sideeq16 <ajikuru3@gmail.com>
2023-06-22 11:51:18 +0000

    added file'

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <monty.h>
+
+/**
+ * main - Monty Byte code reader
+ * @argv: argument values
+ * @argc - count number of argument
+ * Return: 0 or 1
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file");
+	}
+
+	return(0);
+}
