8842ea5c8e9f433e76900ba0c045565367d4f90a
Sideeq16 <126811305+Sideeq16@users.noreply.github.com>
2023-06-23 03:58:02 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,37 +1,112 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "monty.h"
 #include <string.h>
+#include "monty.h"
 
-/**
- * main - Monty Byte code reader
- * @argv: argument values
- * @argc: count number of argument
- * Return: 0 or 1
- */
-int main(int argc, char **argv)
+#define MAX_LINE_LENGTH 1024
+
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+
+instruction_t instructions[] = {
+    {"push", push},
+    {"pall", pall},
+    /* Add more instructions here */
+    {NULL, NULL} // Null terminator for the instructions array
+};
+
+void push(stack_t **stack, unsigned int line_number)
 {
-	FILE *op_file;
-	char *line = NULL;
-	size_t len = 0;
-	int read;
+    // Example implementation of the push instruction
+    // You'll need to modify this according to your needs
+    stack_t *new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-	}
-	op_file = fopen(argv[1],"r");
-	if(op_file == NULL)
-	{
-		printf("error found");	
-	}
+    // Set the value of the new node
+    new_node->n = 42; // Example value, modify according to your needs
 
-	while((read = getline(&line, &len, stdin)) != -1)
-	{
-		printf("Read %s", line);
-	}
+    // Update the pointers of the new node
+    new_node->prev = NULL;
+    new_node->next = *stack;
 
-	fclose(op_file);
-	
-	return (0);
+    // Update the previous pointer of the existing top node (if any)
+    if (*stack != NULL)
+        (*stack)->prev = new_node;
+
+    // Update the stack pointer to point to the new node
+    *stack = new_node;
+}
+
+void pall(stack_t **stack, unsigned int line_number)
+{
+    // Example implementation of the pall instruction
+    // You'll need to modify this according to your needs
+    stack_t *current = *stack;
+    while (current != NULL)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
+}
+
+void execute_instruction(char *opcode, stack_t **stack, unsigned int line_number)
+{
+    int i = 0;
+    while (instructions[i].opcode != NULL)
+    {
+        if (strcmp(opcode, instructions[i].opcode) == 0)
+        {
+            instructions[i].f(stack, line_number);
+            return;
+        }
+        i++;
+    }
+
+    fprintf(stderr, "L%u: unknown instruction %s\n", line_number, opcode);
+    exit(EXIT_FAILURE);
+}
+
+void process_file(const char *file_path)
+{
+    FILE *file = fopen(file_path, "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", file_path);
+        exit(EXIT_FAILURE);
+    }
+
+    char line[MAX_LINE_LENGTH];
+    char *opcode;
+    unsigned int line_number = 1;
+    stack_t *stack = NULL;
+
+    while (fgets(line, sizeof(line), file) != NULL)
+    {
+        opcode = strtok(line, " \t\n");
+
+        if (opcode != NULL && opcode[0] != '#') // Ignore empty lines and comments
+            execute_instruction(opcode, &stack, line_number);
+
+        line_number++;
+    }
+
+    fclose(file);
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        return EXIT_FAILURE;
+    }
+
+    const char *file_path = argv[1];
+    process_file(file_path);
+
+    return EXIT_SUCCESS;
 }
